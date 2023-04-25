cc716d507c67533ac121f51fbc100d18f26b6a7b
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-04-25 22:50:32 +0200

    Update server code to read port from .env file

--- dbtest.js
+++ dbtest.js
@@ -1,19 +1,20 @@
-const mysql = require("mysql");
+const mysql = require('mysql');
+require('dotenv').config();
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "share-a-meal-programmeren-4",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 connection.connect();
 
 connection.query(
-  "SELECT name, id FROM meal;",
+  'SELECT name, id FROM meal;',
   function (error, results, fields) {
     if (error) throw error;
-    console.log("The solution is: ", results[0]);
+    console.log('The solution is: ', results[0]);
   }
 );
 
--- share_a_meal_server/README.md
+++
@@ -1,1 +0,0 @@
-# programmeren_4
