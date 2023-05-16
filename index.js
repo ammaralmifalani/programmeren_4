a5cf30f841d9f2c93e615dcb1076ee300e3fa0af
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-05-16 21:13:28 +0200

    update login, debuggen

--- index.js
+++ index.js
@@ -18,21 +18,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Refer to routes defined in userRouter
-app.use('/api/user', userRouter);
-app.use('/api/meal', mealRouter);
-app.use('/api/auth', authRouter);
-
-// Catch all routes and log their method and URL
-app.use('*', (req, res) => {
-  logger.warn('Invalid endpoint called: ', req.path);
-  res.status(404).json({
-    status: 404,
-    message: 'Endpoint not found',
-    data: {},
-  });
-});
-
 // Define a route for server info
 app.get('/api/info', (req, res) => {
   res.status(200).json({
@@ -51,6 +36,11 @@ app.get('/', (req, res) => {
   res.send('welcome to server API van de share a meal');
 });
 
+// Refer to routes defined in userRouter
+app.use('/api/user', userRouter);
+app.use('/api/meal', mealRouter);
+app.use('/api/auth', authRouter);
+
 // Catch all other routes that do not match any endpoints
 app.use('*', (req, res) => {
   logger.warn('Invalid endpoint called: ', req.path);
@@ -63,16 +53,17 @@ app.use('*', (req, res) => {
 
 // Express error handler
 app.use((error, req, res, next) => {
-  logger.error(error.status, error.message);
   res.status(error.status).json({
     status: error.status,
     message: error.message,
     data: {},
   });
 });
+
 // Start the server on the specified port
 app.listen(port, () => {
-  logger.info(` Server API van de share a meal listening on port ${port}`);
+  logger.info(`Server API van de share a meal listening on port ${port}`);
 });
+
 // Export the server for use in tests
 module.exports = app;
